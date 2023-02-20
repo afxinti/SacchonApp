4eb81778eb928eae116ed5f93c0ffca14d83bd2c
ChristinaManoli <dai19181@uom.edu.gr>
2023-02-20 21:41:46 +0200

    Created Patient Model.

---
+++ src/main/java/gr/pfizer/team5/sacchonapp/model/Patient.java
@@ -0,0 +1,29 @@
+package gr.pfizer.team5.sacchonapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String fullName;
+
+    @OneToMany(mappedBy="patient")
+    private List<Consultation> consultations;
+
+
+}
+
+
