48d4a48efd3015af958db08a2eb1c035f8fd9fab
Afroditi Xinti <102307107+afxinti@users.noreply.github.com>
2023-02-20 15:40:15 +0200

    Initial commit

---
+++ src/main/java/gr/pfizer/team5/sacchonapp/SacchonAppApplication.java
@@ -0,0 +1,13 @@
+package gr.pfizer.team5.sacchonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacchonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SacchonAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/pfizer/team5/sacchonapp/SacchonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.pfizer.team5.sacchonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacchonAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
