f550687145b69d3542d54f865d7d44d4e4f00120
Eleni <elena.v.kalla@gmail.com>
2023-02-21 13:09:46 +0200

    Small Change in BGL Repository

--- src/main/java/gr/pfizer/team5/sacchonapp/repository/BGLRepository.java
+++ src/main/java/gr/pfizer/team5/sacchonapp/repository/BGLRepository.java
@@ -4,7 +4,12 @@ import gr.pfizer.team5.sacchonapp.model.BloodGlucoseLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BGLRepository extends JpaRepository<BloodGlucoseLevel,Integer> {
+//    List<BloodGlucoseLevel> findByDateBetween (LocalDate start, LocalDate end);
+
 
 }
