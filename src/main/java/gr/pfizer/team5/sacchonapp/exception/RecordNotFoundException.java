a94d885e790dff74199ef32713a14c2ce00d7582
Eleni <elena.v.kalla@gmail.com>
2023-02-21 11:02:57 +0200

    RecordNotFoundException Implementation

---
+++ src/main/java/gr/pfizer/team5/sacchonapp/exception/RecordNotFoundException.java
@@ -0,0 +1,8 @@
+package gr.pfizer.team5.sacchonapp.exception;
+
+public class RecordNotFoundException extends Exception{
+    public RecordNotFoundException(String description){
+        super(description);
+    }
+
+}
