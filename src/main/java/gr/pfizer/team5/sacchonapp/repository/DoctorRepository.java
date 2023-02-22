3cbb2549606b95f7a0259a20b1cf6048a48f632b
ChristinaManoli <dai19181@uom.edu.gr>
2023-02-22 23:12:19 +0200

    created a JPQL Query method to det the IDs of all doctors.

--- src/main/java/gr/pfizer/team5/sacchonapp/repository/DoctorRepository.java
+++ src/main/java/gr/pfizer/team5/sacchonapp/repository/DoctorRepository.java
@@ -1,9 +1,17 @@
 package gr.pfizer.team5.sacchonapp.repository;
 
+import gr.pfizer.team5.sacchonapp.model.Consultation;
 import gr.pfizer.team5.sacchonapp.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+    @Query(value = "Select d.id from Doctor d")
+    List<Integer> getAllDoctorIds();
+
 }
