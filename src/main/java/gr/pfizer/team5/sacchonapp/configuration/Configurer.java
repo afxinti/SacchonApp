b8bafb18cdb63c8307613964fb800354301da640
Eleni <elena.v.kalla@gmail.com>
2023-02-26 21:40:46 +0200

    change in configurer for using delete methods

--- src/main/java/gr/pfizer/team5/sacchonapp/configuration/Configurer.java
+++ src/main/java/gr/pfizer/team5/sacchonapp/configuration/Configurer.java
@@ -15,7 +15,9 @@ public class Configurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                        .allowedOrigins("*");
+                        .allowedOrigins("*")
+                        .allowedMethods("GET","POST",  "PUT", "DELETE");
+
             }
         };
     }
