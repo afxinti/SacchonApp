6757195084c8711d3b3220ebc91a3f6bbc2afdf8
Eleni <elena.v.kalla@gmail.com>
2023-03-01 11:21:04 +0200

    Correction after merge

--- SacchonFrontEnd/src/app/app.module.ts
+++ SacchonFrontEnd/src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MyAccountComponent } from './my-account/my-account.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatisticsBglComponent } from './statistics-bgl/statistics-bgl.component';
 import { StatisticsDciComponent } from './statistics-dci/statistics-dci.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { WarningComponent } from './warning/warning.component';
 import { LastConsoltationComponent } from './last-consoltation/last-consoltation.component';
 import { LoadingComponent } from './loading/loading.component';
 import { BglComponent } from './bgl/bgl.component';
-import { BglEditComponent } from './bgl-edit/bgl-edit.component';
 import { BglAddNewComponent } from './bgl-add-new/bgl-add-new.component';
-
+import { BglEditComponent } from './bgl-edit/bgl-edit.component';
+import { ConsultationsListComponent } from './consultations-list/consultations-list.component';
 
 @NgModule({
   declarations: [
@@ -26,18 +26,17 @@ import { BglAddNewComponent } from './bgl-add-new/bgl-add-new.component';
     StatisticsDciComponent,
     WarningComponent,
     LastConsoltationComponent,
-    MyAccountComponent,
     SidebarComponent,
     LoadingComponent,
     BglComponent,
+    BglAddNewComponent,
     BglEditComponent,
-    BglAddNewComponent
+    ConsultationsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
