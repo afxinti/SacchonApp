751b53da7f5f5e57555f4bf2f53c04c81bf8bc21
stavrosantoniadis <sr.antoniadis@gmail.com>
2023-03-02 12:32:18 +0200

    Dci style

--- src/main/java/gr/pfizer/team5/sacchonapp/configuration/InitialConfiguration.java
+++ src/main/java/gr/pfizer/team5/sacchonapp/configuration/InitialConfiguration.java
@@ -23,14 +23,14 @@ public class InitialConfiguration {
             BloodGlucoseLevel bgl1 = new BloodGlucoseLevel(0, LocalDate.of(2023,1,10), LocalTime.of(12,16),12.0,pat2);
             BloodGlucoseLevel bgl2 = new BloodGlucoseLevel(0, LocalDate.of(2022,5,13), LocalTime.of(13,20),10.0,pat1);
             BGLRepository.saveAll(List.of(bgl1, bgl2));
-            DailyCarbonatesIntake dci1= new DailyCarbonatesIntake(0,LocalDate.of(2022,12,2),LocalTime.of(10,22),200.0,pat2);
-            DailyCarbonatesIntake dci2= new DailyCarbonatesIntake(0,LocalDate.of(2022,12,14),LocalTime.of(12,17),190.0,pat1);
+            DailyCarbonatesIntake dci1= new DailyCarbonatesIntake(0,LocalDate.of(2023,2,15),LocalTime.of(10,22),200.0,pat2);
+            DailyCarbonatesIntake dci2= new DailyCarbonatesIntake(0,LocalDate.of(2023,3,14),LocalTime.of(12,17),190.0,pat1);
             DCIRepository.saveAll(List.of(dci1,dci2));
             Doctor doc1 = new Doctor(0,"appdoc@cod.gr","aapdoc","werqr2","Panagiota","Deli", Set.of(pat1,pat2),new Users(0,"aapdoc","werqr2", Authority.DOCTOR));
             Doctor doc2 = new Doctor(0,"apap@fka.com","apap","aposl2","Giorgos","Papas",null,new Users(0,"apap","aposl2", Authority.DOCTOR));
             doctorRepository.saveAll(List.of(doc1,doc2));
             Consultation cons1 = new Consultation(0, LocalDate.of(2023,2,20),"xanax",20,"two per day",1,pat1);
-            Consultation cons2 = new Consultation(0, LocalDate.of(2023,1,18),"lyrica",10,"one per day",1,pat2);
+            Consultation cons2 = new Consultation(0, LocalDate.of(2023,1,18),"Lyrica",10,"one per day",1,pat2);
             consultationRepository.saveAll(List.of(cons1,cons2));
             ChiefDoctor chief1 = new ChiefDoctor(0,"bard@lanf.gr","bard","sssoo90","Thanasis","Lelos",new Users("bard","sssoo90",Authority.CHIEF));
             ChiefDoctor chief2 = new ChiefDoctor(0,"ang@lanf.gr","ang","sso90","Aggeliki","Georgiou",new Users("ang","sso90",Authority.CHIEF));
--- src/main/java/gr/pfizer/team5/sacchonapp/controller/ReporterRestController.java
+++ src/main/java/gr/pfizer/team5/sacchonapp/controller/ReporterRestController.java
@@ -10,6 +10,7 @@ import gr.pfizer.team5.sacchonapp.service.DoctorAdviceServices;
 import gr.pfizer.team5.sacchonapp.service.ReporterServices;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
